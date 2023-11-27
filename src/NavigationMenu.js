2ad2db3e466d23240b800f77403ef3ae4ae5f041
chloeimb <chloeimbusch@gmail.com>
2023-11-26 22:35:28 -0500

    menu

--- src/NavigationMenu.js
+++ src/NavigationMenu.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 
 const NavigationMenu = ({ isOpen, toggleMenu }) => (
   <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
-    <Link to="/" onClick={toggleMenu}>Home</Link>
-    <Link to="/art-gallery" onClick={toggleMenu}>Art Gallery</Link>
-    {/* Add more navigation links as needed */}
+    <div className="overlay" onClick={toggleMenu}></div>
+    <div className="menu-content">
+      <Link to="/" onClick={toggleMenu}>Home</Link>
+      <Link to="/art-gallery" onClick={toggleMenu}>Art Gallery</Link>
+      {/* Add more navigation links as needed */}
+    </div>
   </div>
 );
 
